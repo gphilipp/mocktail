ede7984b1f0a048ebc2b01feaaf66016ef90cd25
Sandeep Rawat <srawat@xebia.com>
2012-08-13 10:41:15 +0530

    Whew.... Made it work up to some extent need to do more cleanup

--- plugin-samples/mocktail-orm/src/test/java/org/mocktail/AbstractDbUnitTest.java
+++ plugin-samples/mocktail-orm/src/test/java/org/mocktail/AbstractDbUnitTest.java
@@ -12,56 +12,11 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.After;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public abstract class AbstractDbUnitTest {
 
-<<<<<<< HEAD
-	public void init() throws Exception {
-		System.out.println("Cleaning & inserting data");
-		DatabaseOperation.CLEAN_INSERT.execute(getConnection(), getDataSet());
-	}
 
-	@After
-	public void after() throws Exception {
-		DatabaseOperation.DELETE_ALL.execute(getConnection(), getDataSet());
-	}
-
-	private IDataSet getDataSet() throws Exception {
-		File file = new File(getDataSetFile());
-		return new FlatXmlDataSet(file);
-	}
-
-	private IDatabaseConnection getConnection()
-			throws SQLException {
-		Connection con = getSqlConnection();
-		IDatabaseConnection dbUnitCon = new DatabaseConnection(con);
-		return dbUnitCon;
-	}
-
-	protected int getRowCountsForQuery(String rowCountQuery) throws SQLException {
-		ResultSet resultSet = getSqlConnection().createStatement().executeQuery(rowCountQuery);
-		resultSet.next();
-		int recordCounts = resultSet.getInt(1);
-		resultSet.close();
-		return recordCounts;
-	}
-
-	protected int getTableTotalRows(String tableName) throws SQLException {
-		return getRowCountsForQuery("select count(*) from " + tableName);
-	}
-
-	protected String getStringValueForQuery(String queryForStringValue) throws SQLException {
-		ResultSet resultSet = getSqlConnection().createStatement().executeQuery(queryForStringValue);
-		resultSet.next();
-		String stringValue = resultSet.getString(1);
-		resultSet.close();
-		return stringValue;
-	}
-
-	protected abstract Connection getSqlConnection() throws SQLException;
-
-	protected abstract String getDataSetFile();
-=======
     public void init() throws Exception {
         System.out.println("Cleaning & inserting data");
         DatabaseOperation.CLEAN_INSERT.execute(getConnection(), getDataSet());
@@ -111,5 +66,4 @@ public abstract class AbstractDbUnitTest {
     protected abstract Connection getSqlConnection() throws SQLException;
 
     protected abstract String getDataSetFile();
->>>>>>> dc038b3d24a102aa117552e10a66b0a3a77b5284
 }
